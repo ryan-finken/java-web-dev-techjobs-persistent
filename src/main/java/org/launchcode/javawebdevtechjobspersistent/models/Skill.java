f1993a10e7489d941c41478a06a5b2b4d605ec1d
Ryan Finken <ryanfinken338@gmail.com>
2021-07-10 12:13:39 -0500

    created a many to many relationship between Job and Skill, changed HomeController and ListController to pass in needed data to their view templates

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,8 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -10,6 +13,9 @@ public class Skill extends AbstractEntity {
     @NotBlank
     @Size(max = 255)
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
     public Skill() {
     }
